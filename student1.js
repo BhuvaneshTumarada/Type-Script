887f1eb4198ed77b74469bb85a3c221c7be34473
BHUVANESHTECH22 <tumaradabhuvanesh9@gmail.com>
2026-08-17 17:51:43 +0530

    Add files via upload

---
+++ student1.js
@@ -0,0 +1,13 @@
+"use strict";
+const student = {
+    id: 101,
+    name: "Bhuvanesh",
+    course: "CSE",
+    cgpa: 8.5,
+    skills: [
+        "Java",
+        "React",
+        "TypeScript"
+    ]
+};
+console.log(student);
