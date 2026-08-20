e8e4fc8a02c9b867c7578d5b3c997bdaba5686d4
BHUVANESHTECH22 <tumaradabhuvanesh9@gmail.com>
2026-08-20 17:51:31 +0530

    Delete student.ts

--- student.ts
+++
@@ -1,8 +0,0 @@
-let studentName: string = "Bhuvanesh";
-let age: number = 22;
-let course: string = "CSE";
-let isGraduated: boolean = false;
-console.log(studentName);
-console.log(age);
-console.log(course);
-console.log(isGraduated);
