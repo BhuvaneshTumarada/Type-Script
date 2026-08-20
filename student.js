bb19fcc0141070ac82164e305d6f45d4794e8889
BHUVANESHTECH22 <tumaradabhuvanesh9@gmail.com>
2026-08-20 17:51:17 +0530

    Delete student.js

--- student.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-let studentName = "Bhuvanesh";
-let age = 22;
-let course = "CSE";
-let isGraduated = false;
-console.log(studentName);
-console.log(age);
-console.log(course);
-console.log(isGraduated);
