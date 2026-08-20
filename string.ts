c217cc59a1cc8dd350d94d076503fc5e360041f0
BHUVANESHTECH22 <tumaradabhuvanesh9@gmail.com>
2026-08-20 17:51:03 +0530

    Delete string.ts

--- string.ts
+++
@@ -1,7 +0,0 @@
-let firstName: string = "Bhuvanesh";
-let lastName: string = "Tumarada";
-let fullName: string = `${firstName} ${lastName}`;
-console.log("Full Name:", fullName);
-console.log("Length:", fullName.length);
-console.log("Uppercase:", fullName.toUpperCase());
-console.log("Lowercase:", fullName.toLowerCase());
